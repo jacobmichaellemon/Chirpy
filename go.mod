8b496e847eb62cf3c6f83776b4742fcd2503b3e0
jacobmichaellemon <jacobmichaellemon@gmail.com>
2026-04-15 22:52:24 -0700

    project setup, server serves basic html and assets

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Chirpy
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main () {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
+
+	server := http.Server{
+		Handler:	mux,
+		Addr:		":8080",
+	}
+	server.ListenAndServe()
+}
